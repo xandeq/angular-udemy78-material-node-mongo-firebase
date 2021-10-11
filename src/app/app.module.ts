c002c4d5d21da2c677aac3440e3158c87cc279e3
schenkeralexandre2021@gmail.com <schenkeralexandre2021@gmail.com>
2021-10-11 06:23:24 -0300

    Até aula 11 - Material Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MyFirstComponent } from './myfirst.component';
+import { MySecondComponent } from './my-second/my-second.component';
+import { MyThirdComponent } from './my-second/my-third/my-third.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIcon, MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+ declarations: [AppComponent, MyFirstComponent, MySecondComponent, MyThirdComponent],
+ imports: [BrowserModule, BrowserAnimationsModule, MatButtonModule, MatIconModule],
+ providers: [],
+ bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: '.myfirst',
+  styleUrls: ['myfirst.component.css'],
+  templateUrl: 'myfirst.component.html'
+})
+
+export class MyFirstComponent {
+
+}
